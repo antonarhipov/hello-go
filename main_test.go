16e313e1b05fdfcf23359b9da191121ffada8a52
Anton Arhipov <ant.arhipov@gmail.com>
2019-08-03 13:09:32 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -8,10 +8,10 @@ func TestSum1(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
-//
-//func TestSum2(t *testing.T) {
-//	total := Sum(5, 5)
-//	if total != 11 {
-//		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-//	}
-//}
+
+func TestSum2(t *testing.T) {
+	total := Sum(5, 5)
+	if total != 11 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+}
